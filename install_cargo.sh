2c3645ea09e0d8a04903c09cb1361df6da313e3b
Björn Urban <burban@inovex.de>
2026-01-04 21:23:33 +0100

    fix: linking on ubuntu

--- install_cargo.sh
+++ install_cargo.sh
@@ -3,4 +3,5 @@ cargo install starship --locked
 cargo install eza
 cargo install atac --locked
 cargo install rainfrog
-cargo install spotify-tui
+cargo install atuin
+cargo install argo install spotify-tui
