928f5a9e1485fff99c97b5b6fe66c110bad3be2d
Björn Urban <burban@inovex.de>
2026-03-13 22:19:57 +0100

    fix: add github scheme and update sketchybar

--- config/sketchybar/helpers/init.lua
+++ config/sketchybar/helpers/init.lua
@@ -1,4 +1,17 @@
--- Add the sketchybar module to the package cpath
-package.cpath = package.cpath .. ";/Users/" .. os.getenv("USER") .. "/.local/share/sketchybar_lua/?.so"
+local source = debug.getinfo(1, "S").source
+local helpers_dir = source:sub(1, 1) == "@" and source:sub(2):match("(.*/)") or nil
+local home_dir = os.getenv("HOME")
 
-os.execute("(cd helpers && make)")
+if not home_dir and source:sub(1, 1) == "@" then
+  home_dir = source:sub(2):match("^(.-)/%.config/")
+end
+
+if home_dir then
+  package.cpath = package.cpath .. ";" .. home_dir .. "/.local/share/sketchybar_lua/?.so"
+end
+
+if helpers_dir then
+  os.execute("(cd " .. string.format("%q", helpers_dir) .. " && make)")
+else
+  os.execute("(cd helpers && make)")
+end
